5df6bb2eeda6caf530360712efadb1c075fab35d
ChristoferSKMJ <ChristoferSKMJ@users.noreply.github.com>
2015-05-18 10:19:32 +0200

    Add text box form for comments

---
+++ 150505/WindowsFormsForecastLactalis/WindowsFormsForecastLactalis/TextBoxForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsForecastLactalis
+{
+    partial class TextBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.buttonSaveInfo = new System.Windows.Forms.Button();
+            this.buttonCloseInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInfo
+            // 
+            this.richTextBoxInfo.Location = new System.Drawing.Point(0, 1);
+            this.richTextBoxInfo.Name = "richTextBoxInfo";
+            this.richTextBoxInfo.Size = new System.Drawing.Size(517, 167);
+            this.richTextBoxInfo.TabIndex = 1;
+            this.richTextBoxInfo.Text = "";
+            this.richTextBoxInfo.TextChanged += new System.EventHandler(this.richTextBoxInfo_TextChanged);
+            // 
+            // buttonSaveInfo
+            // 
+            this.buttonSaveInfo.Location = new System.Drawing.Point(301, 174);
+            this.buttonSaveInfo.Name = "buttonSaveInfo";
+            this.buttonSaveInfo.Size = new System.Drawing.Size(100, 40);
+            this.buttonSaveInfo.TabIndex = 2;
+            this.buttonSaveInfo.Text = "Gem";
+            this.buttonSaveInfo.UseVisualStyleBackColor = true;
+            this.buttonSaveInfo.Click += new System.EventHandler(this.buttonSaveInfo_Click);
+            // 
+            // buttonCloseInfo
+            // 
+            this.buttonCloseInfo.Location = new System.Drawing.Point(407, 174);
+            this.buttonCloseInfo.Name = "buttonCloseInfo";
+            this.buttonCloseInfo.Size = new System.Drawing.Size(100, 40);
+            this.buttonCloseInfo.TabIndex = 3;
+            this.buttonCloseInfo.Text = "Luk";
+            this.buttonCloseInfo.UseVisualStyleBackColor = true;
+            this.buttonCloseInfo.Click += new System.EventHandler(this.buttonCloseInfo_Click);
+            this.buttonCloseInfo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonCloseInfo_KeyDown);
+            // 
+            // TextBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 235);
+            this.Controls.Add(this.buttonCloseInfo);
+            this.Controls.Add(this.buttonSaveInfo);
+            this.Controls.Add(this.richTextBoxInfo);
+            this.Name = "TextBoxForm";
+            this.Text = "Information";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TextBoxForm_FormClosing);
+            this.Load += new System.EventHandler(this.TextBoxForm_Load);
+            //this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxForm_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.Button buttonSaveInfo;
+        private System.Windows.Forms.Button buttonCloseInfo;
+    }
+}
---
+++ 150505/WindowsFormsForecastLactalis/WindowsFormsForecastLactalis/TextBoxForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsForecastLactalis
+{
+    public partial class TextBoxForm : Form
+    {
+        Form1 Form1Instance;
+        public int desiredStartLocationX;
+        public int desiredStartLocationY;
+        private string unchangedText;
+        public TextBoxForm()
+        {
+            desiredStartLocationX = 0;
+            desiredStartLocationY = 0;
+
+            InitializeComponent();
+            MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Form2_Load(object sender, System.EventArgs e)
+        {
+            this.SetDesktopLocation(desiredStartLocationX, desiredStartLocationY);
+        }
+
+        private void TextBoxForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SetInfoText(Form1 formInstance, string comment, string header)
+        {
+            richTextBoxInfo.Text = comment;
+            Form1Instance = formInstance;
+            this.Text = "Info " + header;
+            unchangedText = comment;
+        }
+
+        private void richTextBoxInfo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCloseInfo_Click(object sender, EventArgs e)
+        {
+            CloseCommentWindow();
+        }
+
+        private void buttonSaveInfo_Click(object sender, EventArgs e)
+        {
+            if (unchangedText != richTextBoxInfo.Text)
+            {
+                Form1Instance.SetProductComment(richTextBoxInfo.Text);
+            }
+            this.Hide();
+        }
+
+        private void TextBoxForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCommentWindow();
+            e.Cancel = true;
+
+        }
+
+
+        public void SetNextLocation(Point pos)
+        {
+            // here store the value for x & y into instance variables
+            this.desiredStartLocationX = pos.X;
+            this.desiredStartLocationY = pos.Y;
+            Load += new EventHandler(Form2_Load);
+        }
+
+        internal void FocusTextBox()
+        {
+            richTextBoxInfo.Focus();
+            if (richTextBoxInfo.Text == "Comment")
+            {
+                richTextBoxInfo.Select(0, richTextBoxInfo.Text.Length);
+            }
+            else
+            {
+                richTextBoxInfo.Select(richTextBoxInfo.Text.Length, 0);
+            }
+        }
+
+        private void buttonCloseInfo_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                CloseCommentWindow();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void CloseCommentWindow()
+        {
+            if (unchangedText == richTextBoxInfo.Text)
+            {
+                //Text not changed close
+                this.Hide();
+            }
+            else
+            {
+                //Text changed. Ask before closing
+                DialogResult dialogResult = MessageBox.Show("Do you really want to close without saving?", "Warning", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    //do something
+                    this.Hide();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    //do something else
+                }
+            }
+
+        }
+    }
+}
